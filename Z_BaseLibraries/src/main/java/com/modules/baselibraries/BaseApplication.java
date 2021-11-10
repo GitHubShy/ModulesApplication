0935411228607ce403ba7368e73f1fb824bdbaee
SHY <shy@192.168.50.189>
2021-11-10 16:48:19 +0800

    Test .aar

--- Z_BaseLibraries/src/main/java/com/modules/baselibraries/BaseApplication.java
+++ Z_BaseLibraries/src/main/java/com/modules/baselibraries/BaseApplication.java
@@ -2,10 +2,14 @@ package com.modules.baselibraries;
 
 import android.app.Application;
 
+import com.modules.testjni.Test;
+
+
 public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         LogManager.init();
+        LogManager.d(new Test().stringFromJNI());
     }
 }
