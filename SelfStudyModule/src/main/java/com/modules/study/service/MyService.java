ca2272a6f48e4f549875339a9e275170035d69b5
shiyao <147573224@qq.com>
2022-01-21 14:22:07 +0800

    自启动

--- SelfStudyModule/src/main/java/com/modules/study/service/MyService.java
+++ SelfStudyModule/src/main/java/com/modules/study/service/MyService.java
@@ -2,11 +2,37 @@ package com.modules.study.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.modules.baselibrary.LogProxy;
+import com.socks.library.KLog;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 public class MyService extends Service {
+
+    // 通过静态方法创建ScheduledExecutorService的实例
+    private ScheduledExecutorService mScheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+    private Handler mHandler;
+
+    private Runnable mRunnable;
+
+    private ThreadFactory threadFactory = r -> new Thread() {
+        @Override
+        public void run() {
+            r.run();
+        }
+    };
+
     public MyService() {
     }
 
@@ -14,11 +40,29 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         LogProxy.d("oncreate");
+        mHandler = new Handler(Looper.myLooper());
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Log.d("shy","I am a runnable");
+            }
+        };
+        mHandler.postDelayed(mRunnable,5000);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        LogProxy.d("onStartCommand");
+        LogProxy.d("onStartCommand"+Thread.currentThread().getName());
+//        mHandler.removeCallbacksAndMessages(null);
+//        // 延时任务
+//        thread = threadFactory.newThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                LogProxy.d("" +
+//                        "mScheduledExecutorService");
+//            }
+//        });
+//        mScheduledExecutorService.schedule(thread, 10, TimeUnit.SECONDS);
         return super.onStartCommand(intent, flags, startId);
     }
 
